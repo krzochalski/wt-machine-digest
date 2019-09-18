fd573d7b14542f4085d3acb881be3d3f1ea0fd00
Piotr Krzoska <piotr.krzoska@bitnoi.se>
2019-09-19 01:33:54 +0200

    Added some USA tanks

--- src/services/enums.service.tsx
+++ src/services/enums.service.tsx
@@ -1,5 +1,8 @@
 export class EnumsService {
-  static getEnumValue(driver: string, enumToIterate: object) {
-    return Object.values(enumToIterate).find(key => key);
+  static getEnumValue(property: string, enumToIterate: object) {
+    // @todo: get some fucking sleep and find a workaround
+    // @ts-ignore
+    const key: keyof typeof enumToIterate = Object.keys(enumToIterate).find(key => key === property);
+    return key !== undefined ? enumToIterate[key] : '';
   }
 }
